dcdd30f7e9bfa1d66b7b8616bd9fa5ad69257034
Stella0711 <crystal.quigley@gmail.com>
2026-05-13 09:26:44 -0500

    taking notes on jss

---
+++ script.js
@@ -0,0 +1,46 @@
+
+// ------>>LOOPS<<-------------
+
+// while (count <= 100) {
+//  console.log(count)
+//  count = count + 1;
+// }
+
+// for (let i = 1; i <= 20; ++i) {
+    // if (i % 3 === 0 && i % 5 === 0) {
+        // console.log(`${i} -> Frontend Simplified`)
+    // }
+    // else if (i % 3 === 0) {
+        // console.log(`${i} -> Frontend`)
+    // }
+    // else if (i % 5 === 0) {
+        // console.log(`${i} -> Simplified`)
+    // }
+    // else {
+        // console.log(`${i} -> ${i}`)
+    // *}
+// }
+
+
+// const str = "Get Focused"
+
+// for (let i = 0; i < str.length; ++i) {
+    // console.log(str[i])
+// }
+
+// -----FUNCTIONS-------
+
+// Function definition
+// function welcomePersonToFES (firstName, lastName) {
+    // console.log(`Welcome to FES, ${firstName} ${lastName}`)
+// }
+
+// Call the function
+// welcomePersonToFES(`David`, `Bragg`);
+
+function celsiusToFahrenheit (celsius, fahrenheit)
+    return celsius * 1.8 + 32
+
+console.log(celsiusToFahrenheit(celsius, fahrenheit))
+
+    
