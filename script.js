4f35eb07bbe9dd43ca68a1b28cc01976c5028d1c
Stella0711 <crystal.quigley@gmail.com>
2026-05-14 09:22:34 -0500

    array notes for jss

--- script.js
+++ script.js
@@ -50,10 +50,72 @@
 
 // SECOND OPTION ------------
 
-const celsiusToFahrenheit = (celsius) => {
-    return celsius * 1.8 + 32
-}
+// const celsiusToFahrenheit = (celsius) => {
+    // return celsius * 1.8 + 32
+// }
 
-console.log(celsiusToFahrenheit(0))
-console.log(celsiusToFahrenheit(10))
-console.log(celsiusToFahrenheit(30))
+// console.log(celsiusToFahrenheit(0))
+// console.log(celsiusToFahrenheit(10))
+// console.log(celsiusToFahrenheit(30))
+
+// -------------ARRAYS--------------
+
+// ** let arr = [20, 30, 40, 50, 100]
+
+// First element of array:
+// console.log(arr[0])
+
+//  Last element of array:
+// console.log(arr[arr.length - 1])
+
+// Add element onto end of array:
+// arr.push(200)
+
+// console.log(arr);
+
+// **** MEMORIZE THIS SYNTAX:
+// arr.filter() => {}
+
+// ----- FILTER METHOD --> SHORT VERSION -----
+
+// let newArr = arr.filter(element => element < 50)
+
+// console.log(newArr)
+
+// ---- FILTER METHOD --> LONG VERSION ------- 
+
+// let newArr = arr.filter((element) => {
+    // console.log(element)
+    // if (element <50) {
+        // return true;
+    // }
+// })
+
+// let grades = ['A+', 'A', 'FAIL']
+
+// let goodGrades = grades.filter(element => element !== 'FAIL')
+
+// console.log(goodGrades)
+
+// --- EXAMPLE of LOOP METHOD with if statement ---
+
+// let grades = ['A+', 'A', 'FAIL']
+
+// let goodGrades = []
+
+// for (let i = 0; i < grades.length; ++i) {
+    // if (grades[i] !== 'FAIL') {
+    //   goodGrades.push(grades[i]);
+    // }
+// }
+
+// console.log(goodGrades);
+
+// --- EXAMPLE of *MAP* ARRAY METHOD ---
+
+let dollars = [1 ,5 ,10, 3];
+
+let cents = dollars.map(element => element * 100)
+
+console.log(cents)
+
